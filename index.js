db42d7386c767f67de3b1ec6a79f7e79d3ad4d61
lyw <lyw@lywdeMacBook-Pro.local>
2018-07-12 18:32:32 +0800

    sdfsdf

--- index.js
+++ index.js
@@ -1,58 +1,120 @@
 var https = require('https')
-var mysql = require('mysql')
 var fs = require('fs')
 var path = require('path')
-
-const db = mysql.createPool({
-    host: '122.152.219.175',
-    user: 'root',
-    password: 'root',
-    database: 'blog'
-})
+var _json = require('./url')
 
 const resourcePath = path.join(__dirname, './resource')
-var start_num = parseInt(process.argv[2])//从第几部开始下
-var amount = parseInt(process.argv[3] || 30)//下载多少部电影，默认30部
+var start_num = parseInt(process.argv[2] || 1)//从第几部开始下 或是 下载最新的前几页
+var amount = parseInt(process.argv[3])//下载多少部电影
+
 fs.exists(resourcePath, exists => {
     !exists && fs.mkdirSync(resourcePath)
 })
-if (start_num) {
-    var i=start_num;
-    while (i < (start_num + amount)) {
-        var path_num = start_num < 1000 ? 0 : 1000;
-        var movie_url = `https://a3c3b3.com//${path_num}/${i}/${i}.mp4`
-        download(movie_url, i)
-        i++
-        
+
+class Reptile {
+    constructor() {
+        this.resourcePath = path.join(__dirname, './resource')
+        this.start_num = parseInt(process.argv[2] || 1)//从第几部开始下 或是 下载最新的前几页
+        this.amount = parseInt(process.argv[3])//下载多少部电影
     }
-} else {
-    db.query(`SELECT * FROM av_table`, (err, data) => {
-        if (err) console.error(err);
-        var _data = data
-        _data.forEach((item, index) => {
+
+    async get_json() {
+        return await _json(this.start_num)
+    }
+    //模式1，选择第几部 往后下载多少部
+    async  model_1() {
+        var i = start_num;
+        while (i < (start_num + amount)) {
+            var path_num = start_num < 1000 ? 0 : 1000;
+            var movie_url = `https://a3c3b3.com//${path_num}/${i}/${i}.mp4`
+            this.download(movie_url, i)
+            i++
+        }
+    }
+    //模式2，下载最新的前几页的所有视频
+    async model_2() {
+        let _data = await this.get_json()
+        console.log(_data);
+        
+        _data.forEach(item => {
             let av_url = item.av_url
             let av_name = item.av_name
             var movie_num = av_url.match(/\/\d{1,5}/g)[0].slice(1)
             var path_num = movie_num < 1000 ? 0 : 1000;
             var movie_url = `https://a3c3b3.com//${path_num}/${movie_num}/${movie_num}.mp4`
-            download(movie_url, index)
+            this.download(movie_url,av_name)
         })
-    });
-
+    }
+    async  models() {
+        fs.exists(resourcePath, exists => {
+            if (exists) {
+                start_num && amount ? this.model_1() : this.model_2()
+            }
+        })
+    }
+    download(movie_url, index, callback) {
+        https.get(movie_url, res => {
+            const writeStream = fs.createWriteStream(path.join(__dirname, 'resource', `${index}.mp4`))
+            res.setEncoding('binary')
+            res.on('data', chunk => {
+                writeStream.write(chunk, 'binary')
+            })
+            res.on('end', () => {
+                console.log(`${index}.mp4 下载成功！`);
+                callback && callback()
+            })
+        })
+    }
 }
 
+new Reptile().models()
 
 
-function download(movie_url, index, callback) {
-    https.get(movie_url, res => {
-        const writeStream = fs.createWriteStream(path.join(__dirname, 'resource', `${index}.mp4`))
-        res.setEncoding('binary')
-        res.on('data', chunk => {
-            writeStream.write(chunk, 'binary')
-        })
-        res.on('end', () => {
-            console.log(`${index}.mp4 下载成功！`);
-            callback && callback()
-        })
-    })
-}
+//模式1，选择第几部 往后下载多少部
+// async function model_1() {
+//     var i = start_num;
+//     while (i < (start_num + amount)) {
+//         var path_num = start_num < 1000 ? 0 : 1000;
+//         var movie_url = `https://a3c3b3.com//${path_num}/${i}/${i}.mp4`
+//         download(movie_url, i)
+//         i++
+//     }
+// }
+
+// //模式2，下载最新的前几页的所有视频
+// async function model_2() {
+//     let _data = await get_json()
+//     console.log(_data.length)
+//     _data.forEach(item => {
+//         let av_url = item.av_url
+//         let av_name = item.av_name
+//         var movie_num = av_url.match(/\/\d{1,5}/g)[0].slice(1)
+//         var path_num = movie_num < 1000 ? 0 : 1000;
+//         var movie_url = `https://a3c3b3.com//${path_num}/${movie_num}/${movie_num}.mp4`
+//     })
+// }
+
+// async function models() {
+//     fs.exists(resourcePath, exists => {
+//         if (exists) {
+//             start_num && amount ? model_1() : model_2()
+//         }
+//     })
+// }
+
+// models()
+
+
+// function download(movie_url, index, callback) {
+//     https.get(movie_url, res => {
+//         const writeStream = fs.createWriteStream(path.join(__dirname, 'resource', `${index}.mp4`))
+//         res.setEncoding('binary')
+//         res.on('data', chunk => {
+//             writeStream.write(chunk, 'binary')
+//         })
+//         res.on('end', () => {
+//             console.log(`${index}.mp4 下载成功！`);
+//             callback && callback()
+//         })
+//     })
+// }
--- url.js
+++ url.js
@@ -1,32 +1,44 @@
 var http = require('http')
 var cheerio = require('cheerio')
-var mysql = require('mysql')
-const db = mysql.createPool({
-    host: '122.152.219.175',
-    user:'root',
-    password:'root',
-    database:'blog'
-})
-const url = 'http://www.992jj.com'
+var fs = require('fs')
+var path = require('path')
 
-http.get(url,res=>{
-    var html;
-    res.on('data',chunk=>{
-        html += chunk
-    })
-    res.on('end',()=>{
-        var $ = cheerio.load(html)
-        $('#list_videos_videos_watched_right_now_items .item').each(function(item){
-            var av_name = $('.title',this).text()
-            var av_url = $('a',this).attr('href')
-            db.query(`INSERT INTO av_table (av_name,av_url) VALUES ('${av_name}','${av_url}')`, err => {
-                if(err){
-                    console.error(err);                
-                }else{
-                    console.log('insert success!');
-                    
-                }
+
+function get_html(url) {
+    return new Promise((resolve, reject) => {
+        http.get(url, res => {
+            var html;
+            res.on('data', chunk => {
+                html += chunk
+            })
+            res.on('end', () => {
+                resolve(html);
             })
         })
+
     })
-})
+}
+
+async function insert_json(pageNum) {
+    var i = 1;
+    let json = [];
+    while (i <= pageNum) {
+        const url = `http://www.992jj.com/?mode=async&function=get_block&block_id=list_videos_most_recent_videos&sort_by=post_date&from=${i}`
+        let _html = await get_html(url)
+        let $ = cheerio.load(_html)
+        $('.margin-fix .item').each(function (item) {
+            var o = {}
+            var av_name = $('a', this).attr('title')
+            var av_url = $('a', this).attr('href')
+            o['av_name'] = av_name
+            o['av_url'] = av_url
+            json.push(o)
+        })
+        i++
+    }
+    return json
+}
+
+
+module.exports = insert_json
+
